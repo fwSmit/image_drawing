d6fddc86be10c08d158bfb4f24cf1ea21afc69cf
fwsmit <fw.smit01@gmail.com>
2017-10-27 22:17:17 +0200

    some small tweaks to make it work

--- source.cpp
+++ source.cpp
@@ -19,23 +19,22 @@ int main()
 
     const string file_name = "rabbit.jpg";
     sf::Image img;
-    if(!img.loadFromFile("images/" + file_name)){
+    if(!img.loadFromFile("../images/" + file_name)){
         return EXIT_FAILURE;
     }
 
     sf::RenderWindow window(sf::VideoMode(img.getSize().x, img.getSize().y), "SFML works!");
     window.setVerticalSyncEnabled(1);
 
-    //sf::CircleShape shape(rad);
-    //vector<sf::CircleShape> circles;
-    //shape.setOrigin(shape.getRadius(),shape.getRadius());
-    //shape.setFillColor(sf::Color::Green);
+    sf::CircleShape shape(rad);
+    vector<sf::CircleShape> circles;
+    shape.setOrigin(shape.getRadius(),shape.getRadius());
     //cout << img.getSize().x << " " << img.getSize().y << endl;
 
 
-    sf::RectangleShape shape(sf::Vector2f (size, size));
-    shape.setOrigin(0.5 * shape.getSize().x, 0.5 * shape.getSize().y * 0.5);
-    vector<sf::RectangleShape> circles;
+    //sf::RectangleShape shape(sf::Vector2f (size, size));
+    //shape.setOrigin(0.5 * shape.getSize().x, 0.5 * shape.getSize().y * 0.5);
+    //vector<sf::RectangleShape> circles;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -48,19 +47,21 @@ int main()
 
         if(i > 10 && rad > 1 && size > decrement) {
             i = 0;
-            cout << "radius change" << endl;
+            //cout << "radius change" << endl;
             //rad -= 1;
             size -= decrement;
-            cout << size << endl;
-            shape.setSize(sf::Vector2f(size, size));
+            //cout << size << endl;
+            //shape.setSize(sf::Vector2f(size, size));
             //shape.setRadius(rad);
         }
         for(int i = 0; i < 100; i ++) {
             sf::Vector2i pos;
-            pos.x = size* randInt(0, window.getSize().x/size);
-            pos.y = size* randInt(0, window.getSize().y/size);
+            //pos.x = size* randInt(0, window.getSize().x/size);
+            //pos.y = size* randInt(0, window.getSize().y/size);
             //sf::Vector2i pos = sf::Mouse::getPosition(window);
             //cout << pos.x << " " << pos.y << endl;
+			pos.x = randInt(0, window.getSize().x);
+			pos.y = randInt(0, window.getSize().y);
             if(pos.x >= 0 && pos.x < img.getSize().x && pos.y >= 0 && pos.y < img.getSize().y) {
                 sf::Color currColor = img.getPixel(pos.x, pos.y);
                 currColor.a = 100;
@@ -84,7 +85,7 @@ int main()
         //window.draw(shape);
         window.display();
         i++;
-        //dcout << "frame " << i++ << endl;
+        //cout << "frame " << i++ << endl;
     }
 
     return 0;
